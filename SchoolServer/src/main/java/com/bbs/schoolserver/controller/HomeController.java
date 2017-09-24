ca793a96253136d051fa085dd078e5f4d58dca35
baibisen41 <m15840972545@163.com>
2017-09-24 22:22:45 +0800

    添加log4j日志

--- SchoolServer/src/main/java/com/bbs/schoolserver/controller/HomeController.java
+++ SchoolServer/src/main/java/com/bbs/schoolserver/controller/HomeController.java
@@ -2,9 +2,11 @@ package com.bbs.schoolserver.controller;
 
 import com.bbs.schoolserver.common.BaseConstant;
 import com.bbs.schoolserver.utils.ForumNumberUtil;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/mvc")
 public class HomeController {
 
+    private static final Logger logger = Logger.getLogger(HomeController.class);
 
     @RequestMapping(value = "/home.do", method = RequestMethod.GET)
     public String showPage() {
@@ -24,6 +27,7 @@ public class HomeController {
 
     @RequestMapping(value = "/checkLogin.do", method = RequestMethod.POST)
     public String checkLogin(HttpSession session, HttpServletRequest request) {
+        logger.info(System.currentTimeMillis());
         String id = request.getParameter("id");
         String pwd = request.getParameter("password");
         int flag = BaseConstant.TEACHER_STATUS;
@@ -31,6 +35,7 @@ public class HomeController {
             session.setAttribute("userid", id);
             session.setAttribute("userflag", flag);
             //    return "redirect:/task/showTaskMessage.do";
+            logger.info(System.currentTimeMillis());
             return "selectPage";
         }
         return "fail";
